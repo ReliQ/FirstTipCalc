9f5c3d519c4f46ef2b83c913a27fab46e80dfd4b
Patrick Reid <versatile.jamrok@gmail.com>
2014-03-28 23:27:07 -0500

    gui+++

--- src/com/reliqartz/firsttipcalc/CalcFragment.java
+++ src/com/reliqartz/firsttipcalc/CalcFragment.java
@@ -96,7 +96,7 @@ public class CalcFragment extends Fragment implements OnSharedPreferenceChangeLi
 	 */
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-		return inflater.inflate(R.layout.calc_fragment, container, false);
+		return inflater.inflate(R.layout.fragment_calc, container, false);
 	}
 	
 	/* (non-Javadoc)
